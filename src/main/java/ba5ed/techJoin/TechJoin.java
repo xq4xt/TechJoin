1371ca77fea26622e03510bbc575811fe2b505d1
0xBa5ed <XtremeQuestXceptionalTranscendence@proton.me>
2024-08-27 07:00:34 -0400

    Def didnt forget to register

--- src/main/java/ba5ed/techJoin/TechJoin.java
+++ src/main/java/ba5ed/techJoin/TechJoin.java
@@ -2,9 +2,12 @@ package ba5ed.techJoin;
 
 import ba5ed.techJoin.commands.CommandManager;
 import ba5ed.techJoin.config.ConfigManager;
-import org.bukkit.plugin.java.JavaPlugin;
-import ba5ed.techJoin.listeners.PlayerListener;
+import ba5ed.techJoin.listeners.PlayerJoinListener;
+import ba5ed.techJoin.listeners.PlayerQuitListener;
+import ba5ed.techJoin.listeners.PlayerMovementListener;
+import ba5ed.techJoin.listeners.PlayerDeathListener;
 import ba5ed.techJoin.util.MessageUtil;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class TechJoin extends JavaPlugin {
 
@@ -18,12 +21,20 @@ public class TechJoin extends JavaPlugin {
         this.messageUtil = new MessageUtil(this);
 
         // Register event listeners
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        registerEvents();
 
         // Register commands
         new CommandManager(this);
 
         getLogger().info("TechJoin has been enabled!");
+    }
+
+    private void registerEvents() {
+        // Register event listeners
+        new PlayerJoinListener(this);
+        new PlayerQuitListener(this);
+        new PlayerMovementListener(this);
+        new PlayerDeathListener(this);
     }
 
     @Override
@@ -35,4 +46,7 @@ public class TechJoin extends JavaPlugin {
         return configManager;
     }
 
+    public MessageUtil getMessageUtil() {
+        return messageUtil;
+    }
 }
