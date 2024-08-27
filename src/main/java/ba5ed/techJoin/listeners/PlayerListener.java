251895aaf8a6f2d1283034c4584a2d7a4433afe7
0xBa5ed <XtremeQuestXceptionalTranscendence@proton.me>
2024-08-27 07:05:04 -0400

    Dont move

--- src/main/java/ba5ed/techJoin/listeners/PlayerListener.java
+++ src/main/java/ba5ed/techJoin/listeners/PlayerListener.java
@@ -5,24 +5,45 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.ChatColor;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
-public class PlayerListener implements Listener {
+import static ba5ed.techJoin.util.Utils.registerPlayer;
+
+public class PlayerListeners implements Listener {
     private final TechJoin plugin;
 
-    public PlayerListener(TechJoin plugin) {
+    public PlayerListeners(TechJoin plugin) {
         this.plugin = plugin;
+        
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String welcomeMessage = plugin.getConfigManager().getWelcomeMessage()
-                .replace("{player}", player.getName());
+        registerPlayer(player);
+    }
 
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', welcomeMessage));
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        // Handle player quit event
+        plugin.getLogger().info(player.getName() + " has left the server.");
+    }
 
-        // event.setJoinMessage(null);
-        // event.setJoinMessage(ChatColor.translateAlternateColorCodes() + player.getName() + " joined the game");
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        // Handle player movement event
+        // Example: track player movement or apply certain effects
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        // Handle player death event
+        plugin.getLogger().info(player.getName() + " has died.");
     }
 }
